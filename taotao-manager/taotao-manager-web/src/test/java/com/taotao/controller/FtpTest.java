87d357cdb5e35f04483980bee7d2464dbefebb2d
limingchao <limingchaosky@163.com>
2017-11-29 23:02:58 +0800

    增加FTPUtils的插件。并作测试通过。

--- taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/FtpTest.java
+++ taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/FtpTest.java
@@ -2,11 +2,15 @@ package com.taotao.controller;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.SocketException;
 
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.junit.Test;
+
+import com.taotao.common.utils.FtpUtil;
 
 public class FtpTest {
 
@@ -19,13 +23,23 @@ public class FtpTest {
 		
 		ftpClient.login("ftpuser", "ftpuser");
 		
-		FileInputStream fileInputStream = new FileInputStream(new File("C:\\Users\\limingchao\\Desktop\\新建文本文档.txt"));
+		FileInputStream fileInputStream = new FileInputStream(new File("C:\\Users\\limingchao\\Desktop\\6.jpg"));
 		
 		ftpClient.changeWorkingDirectory("/home/ftpuser/www/images");
 		
-		ftpClient.storeFile("test.txt", fileInputStream);
+		ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+		
+		ftpClient.storeFile("6.jpg", fileInputStream);
 		
 		ftpClient.logout();
+		
+	}
+	
+	@Test
+	public void ftpUtilsTest() throws Exception {
+		
+		FileInputStream fileInputStream = new FileInputStream(new File("C:\\Users\\limingchao\\Desktop\\6.jpg"));
+		FtpUtil.uploadFile("192.168.157.130", 21, "ftpuser", "ftpuser", "/home/ftpuser/www/images", "/2017", "marry.jpg", fileInputStream);
 		
 	}
 	
